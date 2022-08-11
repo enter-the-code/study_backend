81613b9c2204a61ae3b8291e25bd3e4407dcd9a6
enter-the-code <ssoonngg53@naver.com>
2022-08-11 17:44:06 +0900

    practice 1

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express=require("express");
 const app=express();
-
+const path = require('path');
 const port=8000;
 
+app.set("view engine","ejs");
+app.set('views', path.join(__dirname, 'views')); 
+
+app.use("/public", express.static("static"))
+
+var aaa=["보기1","보기2","보기2","보기4","보기5"]
+
+
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/test.html");
+    res.render("test",{menu:aaa});
+})
+
+app.get("/test1",(req,res)=>{
+    res.render("test1");
+})
+
+app.get("/test3",(req,res)=>{
+    res.render("test3")
 })
 
 app.listen(port,()=>{
